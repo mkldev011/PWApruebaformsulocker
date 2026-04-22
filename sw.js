46dbe663f41ed06c81094dac90e485dbd9a32ac4
mkldev011 <mkldev011@gmail.com>
2026-04-22 12:24:47 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  console.log('PWA de Bypass instalada');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Aquí podrías filtrar tráfico en el futuro
+});
